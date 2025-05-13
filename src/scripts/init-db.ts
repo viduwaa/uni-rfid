8eb0e67f70676ad0267e43e96eb72bcf56b2e37c
Vidula Deneth Salwathura <viduladenethsalwathura@gmail.com>
2025-05-14 00:03:32 +0530

    basic folder structure started & db connection configured

---
+++ src/scripts/init-db.ts
@@ -0,0 +1,39 @@
+// scripts/init-db.ts
+import path from 'path'
+import fs from 'fs'
+import { pool } from '../lib/db.ts'
+
+
+async function initTables(){
+  try {
+    // Read the schema SQL file
+    const schemaPath = path.join(process.cwd(), 'schema.sql')
+    const schemaSql = fs.readFileSync(schemaPath, 'utf8')
+    
+    // Execute the schema SQL
+    await pool.query(schemaSql)
+    
+    // Close the pool
+    await pool.end()
+  } catch (error) {
+    console.error('Error initializing database:', error)
+  }
+}
+
+async function initDb() {
+  try {
+    // Test connection
+    const result = await pool.query('SELECT version()')
+    console.log(result.rows[0].version)
+    
+    //initialize tables
+    await initTables()
+  } catch (error) {
+    console.error('Error initializing database:', error)
+  } finally {
+    console.log('Database schema initialized successfully')
+  }
+}
+
+// Run the initialization
+initDb()
